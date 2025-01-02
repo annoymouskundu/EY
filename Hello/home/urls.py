f4e62fc7c392ff41c189e4c92b88d99fdb2159ad
SwayamKohli <swayamkohli11489@gmail.com>
2025-01-02 16:01:39 +0530

    Updated files
    Schemes files are added , with urls update

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path ,include
 from home import views
+from .views import upload_document, verify_aadhaar  # Import the views
 
 urlpatterns = [
     path("", views.index, name='home'),
@@ -8,5 +9,6 @@ urlpatterns = [
     path("services", views.services, name='services'),
     path("contact/", views.contact, name='contact'),
     path("login/", views.login_view, name='login'),
-
+    path('', upload_document, name='upload_document'),  # Handle requests to '/upload/' for document upload
+    path('verify-aadhaar/', verify_aadhaar, name='verify_aadhaar'),  # Endpoint for Aadhaar verification
 ]
